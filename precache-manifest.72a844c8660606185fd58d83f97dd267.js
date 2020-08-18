b09eab9057ba1fd63ce314a5980d51e52e3805c3
Phonedolly <Phonedolly@users.noreply.github.com>
2020-08-19 01:48:40 +0900

    Updates

---
+++ precache-manifest.72a844c8660606185fd58d83f97dd267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a390b0c9c3e7c24821a52061c57244bc",
+    "url": "/sketch-react/index.html"
+  },
+  {
+    "revision": "8108e8c29da40bb8deb7",
+    "url": "/sketch-react/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7df2727ff50363805e33",
+    "url": "/sketch-react/static/js/2.3934a195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sketch-react/static/js/2.3934a195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8108e8c29da40bb8deb7",
+    "url": "/sketch-react/static/js/main.933d73b4.chunk.js"
+  },
+  {
+    "revision": "72842f99239f6d994b41",
+    "url": "/sketch-react/static/js/runtime-main.4235be55.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/sketch-react/static/media/logo.ee7cd8ed.svg"
+  }
+]);
